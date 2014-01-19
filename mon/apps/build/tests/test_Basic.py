91f75118845a5fbdeeb53a305e30fc29ed5ef2ea
zionist <slaviann@gmail.com>
2014-01-19 12:43:08 +0400

    basic test

---
+++ mon/apps/build/tests/test_Basic.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.test.client import Client
+
+class BasicNavigation(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_can_login(self):
+        # The description should be set in the configuration file
+        responce = self.c.post("/login/", {
+            'username': 'admin',
+            'password': 'admin',
+            'next': '/main/',
+        })
+        self.assertEqual(responce.status_code, 200)
