9c469692e1daf2d5c9ba0f956ad1d3ac29f892d3
Svetlana.Kharina <nan.tyit@gmail.com>
2014-02-14 02:27:56 +0400

    added migrations

---
+++ mon/apps/mo/migrations/0009_auto__chg_field_federalbudget_subvention_performance__add_field_depart.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'FederalBudget.subvention_performance'
+        db.alter_column(u'mo_federalbudget', 'subvention_performance', self.gf('django.db.models.fields.IntegerField')())
+        # Adding field 'DepartamentAgreement.start_year'
+        db.add_column(u'mo_departamentagreement', 'start_year',
+                      self.gf('django.db.models.fields.DateField')(default=datetime.datetime(2014, 2, 13, 0, 0), blank=True),
+                      keep_default=False)
+
+        # Adding field 'DepartamentAgreement.finish_year'
+        db.add_column(u'mo_departamentagreement', 'finish_year',
+                      self.gf('django.db.models.fields.DateField')(default=datetime.datetime(2014, 2, 13, 0, 0), blank=True),
+                      keep_default=False)
+
+
+        # Changing field 'RegionalBudget.subvention_performance'
+        db.alter_column(u'mo_regionalbudget', 'subvention_performance', self.gf('django.db.models.fields.IntegerField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'FederalBudget.subvention_performance'
+        db.alter_column(u'mo_federalbudget', 'subvention_performance', self.gf('django.db.models.fields.IntegerField')(null=True))
+        # Deleting field 'DepartamentAgreement.start_year'
+        db.delete_column(u'mo_departamentagreement', 'start_year')
+
+        # Deleting field 'DepartamentAgreement.finish_year'
+        db.delete_column(u'mo_departamentagreement', 'finish_year')
+
+
+        # Changing field 'RegionalBudget.subvention_performance'
+        db.alter_column(u'mo_regionalbudget', 'subvention_performance', self.gf('django.db.models.fields.IntegerField')(null=True))
+
+    models = {
+        'mo.departamentagreement': {
+            'Meta': {'object_name': 'DepartamentAgreement'},
+            'agreement_type': ('django.db.models.fields.SmallIntegerField', [], {'default': '0', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'finish_year': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2014, 2, 13, 0, 0)', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mo.MO']", 'null': 'True', 'blank': 'True'}),
+            'num': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'start_year': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2014, 2, 13, 0, 0)', 'blank': 'True'}),
+            'subvention': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mo.Subvention']", 'null': 'True', 'blank': 'True'})
+        },
+        'mo.federalbudget': {
+            'Meta': {'object_name': 'FederalBudget'},
+            'adm_coef': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sub_orph_home': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'sub_sum': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'subvention_performance': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'})
+        },
+        'mo.mo': {
+            'Meta': {'object_name': 'MO'},
+            'common_amount': ('django.db.models.fields.FloatField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
+            'common_economy': ('django.db.models.fields.FloatField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
+            'common_percentage': ('django.db.models.fields.FloatField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
+            'common_spent': ('django.db.models.fields.FloatField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
+            'creation_form': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '24', 'null': 'True', 'blank': 'True'}),
+            'has_trouble': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            'home_orphans': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '2048'})
+        },
+        'mo.orphan': {
+            'Meta': {'object_name': 'Orphan'},
+            'age': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'have_home': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_privilege': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'mo.peopleamount': {
+            'Meta': {'object_name': 'PeopleAmount'},
+            'deals': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'future_queue_by_list': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'future_unhome_orphan': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'future_unhome_orphan_14_18': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mo.MO']", 'null': 'True', 'blank': 'True'}),
+            'privilege_people': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'queue_by_list': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'recoverers': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'unhome_orphan': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'unhome_orphan_14_18': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'mo.regionalbudget': {
+            'Meta': {'object_name': 'RegionalBudget'},
+            'adm_coef': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sub_orph_home': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'sub_sum': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'subvention_performance': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'})
+        },
+        'mo.subvention': {
+            'Meta': {'object_name': 'Subvention'},
+            'amount': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'fed_budget': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mo.FederalBudget']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'reg_budget': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mo.RegionalBudget']", 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['mo']
